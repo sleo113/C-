e5c5dcb8958672c0cda04ab9c2a8e3d64b2a58e7
JeUk Park <77144006+sleo113@users.noreply.github.com>
2021-11-14 16:12:07 +0900

    Add files via upload
    21.11.14 C언어 화면에 글을 보이기 위해 #include를 사용하였고 printf문을 사용하여 간단한 자기 소개를 해봄.

---
+++ 11.14reminded-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(void)
+{
+	printf("안녕하세요. \n"); 
+	printf("만나서 반갑습니다. \n");
+	printf("저는 박제욱이라고 합니다. \n");
+	printf("저는 대학교를 다니고 있고 현재 2학년입니다. \n");
+	printf("앞으로 꾸준하게 공부하여 다양한 지식을 가지도록 노력하겠습니다. \n");
+
+	return 0;
+}
